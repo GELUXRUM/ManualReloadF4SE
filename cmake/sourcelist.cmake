eaa52ca3263b9017a831fa7ad1face9ff5ab696f
Geluxrum Lux <geluxrum@gmail.com>
2024-01-03 22:32:15 +0000

    Replace Xbyak with safe_write

--- cmake/sourcelist.cmake
+++ cmake/sourcelist.cmake
@@ -3,8 +3,4 @@ set(SOURCES
 	src/main.cpp
 	src/hooks.cpp
 	src/hooks.h
-	src/xbyak/xbyak.h
-	src/xbyak/xbyak_bin2hex.h
-	src/xbyak/xbyak_mnemonic.h
-	src/xbyak/xbyak_util.h
 )
--- src/hooks.cpp
+++ src/hooks.cpp
@@ -11,31 +11,13 @@ namespace Hooks
 		return 0;
 	}
 
-	struct skipAutoReload : public Xbyak::CodeGenerator
-	{
-		skipAutoReload()
-		{
-			mov(eax, esi);
-			add(rsp, 0xE8);
-			pop(rdi);
-			pop(rsi);
-			pop(rbx);
-			pop(rbp);
-			ret();
-		}
-	};
-	
-
 	void Install(F4SE::Trampoline& trampoline)
 	{
 		REL::Relocation<PlayerControls_DoActionSig> callLocation{ REL::ID(818081), 0x40A };
 		playerControls_DoAction = trampoline.write_call<5>(callLocation.address(), &HookedPlayerControls_DoAction);
-		
-		REL::Relocation<std::uintptr_t> start{ REL::ID(902833), 0x1EC };
-		skipAutoReload code;
-		auto result = trampoline.allocate(code.getSize());
-		REL::safe_write(reinterpret_cast<uintptr_t>(result), code.getCode(), code.getSize());
-		trampoline.write_branch<5>(start.address(), result);
-		
+
+		REL::Relocation<std::uintptr_t> start{ REL::ID(902833), 0x206 };
+		uint8_t bytes[] = { 0x90, 0xE9 }; // replaces near-jump jz `0F 84`
+		REL::safe_write<uint8_t>(start.address(), std::span{ bytes });
 	}
 }
